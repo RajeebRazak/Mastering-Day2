7b50f051d5b31fc5dab0a0fedc8d45faf22a3e3b
RajeebRazak <145429667+RajeebRazak@users.noreply.github.com>
2023-09-24 11:40:44 +0530

    Add files via upload

---
+++ mastering-2.js
@@ -0,0 +1,33 @@
+var fruits = "apple";
+console.log(fruits);
+
+var fruits = "apple is a tasty fruits";
+console.log(fruits.length);
+
+var fruits = "orange is amazing fruit";
+ console.log(fruits.toUpperCase());
+ 
+ var fruits = "MANGO IS A ALSO USED CURRY";
+ console.log(fruits.toLowerCase());
+  
+var fruits = "pineapple";
+ console.log(fruits.charAt(4));
+ 
+var name = "rajeebrazak";
+ console.log(name.split());
+
+var name = "arif alathur";
+ console.log(name.substring());
+ 
+ var name = "kettile pedika";
+  console.log(name.replace("e","a"));
+
+  var myString="palakkad";
+  console.log(myString.indexOf("d"));
+  
+ var myString = "Name";
+  console.log(myString.indexOf("m"));
+
+var name = "  ummini  ";
+ console.log(name.trim());
+  
